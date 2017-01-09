9b96e5699904975a94c754b11beed3b71bec13ea
Ben S <ben.selby@ucalgary.ca>
2017-01-09 12:01:40 -0700

    Corrections to COGDIS

--- scripts/cogdis.js
+++ scripts/cogdis.js
@@ -10,7 +10,6 @@ function get_row( row, raw_data ){
                        raw_data);
 }
 
-
 module.exports = {
     check_data: function(url) {
         
@@ -20,11 +19,6 @@ module.exports = {
         var queries = [];
         
         raw_data = Baby.parseFiles( url, {header: true} );
-//        if (raw_data.errors) {
-//            console.log("Parsed file %s and found the following error:", url);
-//            console.log( "\"", raw_data.errors[0].message, "\"" );
-//        } else
-//            console.log( "Parsed file %s and found no errors.", url );  
             
         data = rf.filter_data_quality(raw_data.data);
         
@@ -33,9 +27,9 @@ module.exports = {
             for (var i=0; i<data.length; i++) {
                 var row_ind = get_row(data[i], raw_data.data);
                 
-                var a_items = ['cogids_01','cogids_02','cogids_03',
-                                'cogids_04','cogids_05','cogids_06',
-                                'cogids_07','cogids_08','cogids_09'];
+                var a_items = ['cogdis_01','cogdis_02','cogdis_03',
+                                'cogdis_04','cogdis_05','cogdis_06',
+                                'cogdis_07','cogdis_08','cogdis_09'];
                 var count = 0;
                 for (j in a_items){
                     if (count==2)
@@ -45,7 +39,13 @@ module.exports = {
                 }
                 if (count==2 && data[i].cogdis_10!='1')
                     queries.push(rf.make_query(data[i], 'Item B', "Should be Yes (1) given 2 A items are 3 or higher", row_ind));        
-                
+                else if (count<2 && data[i].cogdis_10=='1'){
+                    if (count==0)
+                        queries.push(rf.make_query(data[i], 'Item B', "Should be No (0) - no A items are 3 or higher", row_ind));
+                    else        
+                        queries.push(rf.make_query(data[i], 'Item B', "Should be No (0) - only " + count + " A item is 3 or higher", row_ind));
+                }
+                    
                 if (data[i].cogdis_12!='1' && data[i].cogdis_10=='1' && data[i].cogdis_11=='1')                  
                     queries.push(rf.make_query(data[i], 'Item D', "Should be Yes (1) given items B and C are Yes", row_ind));        
             }
